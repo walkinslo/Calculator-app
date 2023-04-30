37757d6f4e67c66b177dad4ceb1d4de16fc96f07
Walkinslo <mark.tkachev.1995@gmail.com>
2023-04-30 21:54:48 +0300

    First commit

---
+++ Calculator/app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.example.calculator;
+
+public class Calculator {
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int mul(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static float div(float num1, float num2) {
+        return num1 / num2;
+    }
+
+}
---
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonAdd, buttonSub, buttonMul, buttonDiv;
+    EditText editTextN1, editTextN2;
+    TextView textView;
+    int num1, num2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAdd = findViewById(R.id.btn_add);
+        buttonSub = findViewById(R.id.btn_sub);
+        buttonMul = findViewById(R.id.btn_mul);
+        buttonDiv = findViewById(R.id.btn_div);
+
+        editTextN1 = findViewById(R.id.number1);
+        editTextN2 = findViewById(R.id.number2);
+        textView = findViewById(R.id.answer);
+
+        buttonAdd.setOnClickListener(this);
+        buttonSub.setOnClickListener(this);
+        buttonMul.setOnClickListener(this);
+        buttonDiv.setOnClickListener(this);
+    }
+
+    public int getIntFromEditText(EditText editText) {
+        if (editText.getText().toString().equals("")) {
+            Toast.makeText(this, "Введите число", Toast.LENGTH_SHORT).show();
+            return 0;
+        } else
+            return Integer.parseInt(editTe`xt.getText().toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        num1 = getIntFromEditText(editTextN1);
+        num2 = getIntFromEditText(editTextN2);
+
+        switch (view.getId()) {
+            case R.id.btn_add:
+                textView.setText("Ответ: " + Calculator.add(num1, num2));
+                break;
+            case R.id.btn_sub:
+                textView.setText("Ответ: " + Calculator.sub(num1, num2));
+                break;
+            case R.id.btn_mul:
+                textView.setText("Ответ: " + Calculator.mul(num1, num2));
+                break;
+            case R.id.btn_div:
+                textView.setText("Ответ: " + Calculator.div(num1, num2));
+                break;
+        }
+
+    }
+}
